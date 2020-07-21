9a18a46c1d9c0edc82b1cd846fe7aa63a56bee3d
Danial Jalaluddin <dan90@live.com.my>
2020-07-21 12:01:01 +0800

    Create msgeng.php

---
+++ msgeng.php
@@ -0,0 +1,37 @@
+<?php
+    return [
+        // home.index
+        "Recently View" => "Recently View",
+        "List Product" => "List Product",
+        "Select your available product here," => "Select your available product here,",
+        "Overview" => "Overview",
+        "SATU Dashboard" => "SATU Dashboard",
+
+        // core
+        "Home" => "Home",
+        "Personnel Management" => "Personnel Management",
+        "Dashboard" => "Dashboard",
+        "Farm Management" => "Farm Management",
+        "Plot Management" => "Plot Management",
+        "Device" => "Device",
+        "Device Raw Reading" => "Device Raw Reading",
+        "Maps" => "Maps",
+        "Operation Management" => "",
+        "My Task" => "My Task",
+        "Inventory" => "Inventory",
+        "Reporting" => "Reporting",
+        "Operation Input" => "Operation Input",
+        "Report" => "Report",
+        "Live Map" => "Live Map",
+        "Asset" => "Asset",
+        "Geofence" => "Geofence",
+        "Mailbox" => "Mailbox",
+        "Postman" => "Postman",
+        "Routing" => "Routing",
+        "Zone" => "Zone",
+        "Map" => "Map"
+
+
+        
+    ]
+?>
