355b5e9586e76f78947de716dd889479b08cbb43
Danial Jalaluddin <dan90@live.com.my>
2020-07-21 12:01:28 +0800

    Create msgmy.php

---
+++ msgmy.php
@@ -0,0 +1,37 @@
+<?php
+    return [
+        // home.index
+        "Recently View" => "Telah Dilihat",
+        "List Product" => "Senarai Produk",
+        "Select your available product here," => "Pilih produk yang ada di sini,",
+        "Overview" => "Gambaran Keseluruhan",
+        "SATU Dashboard" => "SATU Papan pemuka",
+
+        // core
+        "Home" => "Halaman Utama",
+        "Personnel Management" => "Pengurusan Personel",
+        "Dashboard" => "Papan pemuka",
+        "Farm Management" => "Pengurusan Ladang",
+        "Plot Management" => "Pengurusan Plot",
+        "Device" => "Peranti",
+        "Device Raw Reading" => "Bacaan Peranti Asli",
+        "Maps" => "Peta",
+        "Operation Management" => "Pengurusan Operasi",
+        "My Task" => "Tugasan saya",
+        "Inventory" => "Inventori",
+        "Reporting" => "Melaporkan",
+        "Operation Input" => "Input Operasi",
+        "Report" => "Laporan",
+        "Live Map" => "Peta Langsung",
+        "Asset" => "Aset",
+        "Geofence" => "Pagar Geo",
+        "Mailbox" => "Peti Surat",
+        "Postman" => "Posman",
+        "Routing" => "Laluan",
+        "Zone" => "Zon",
+        "Map" => "Peta"
+
+
+        
+    ]
+?>
